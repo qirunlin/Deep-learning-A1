6a0beea001beb1c7cbae48acee2c21e6dd3bccd3
qirunlin <88021442+qirunlin@users.noreply.github.com>
2024-09-19 19:55:06 +0930

    Add files via upload

--- perceprton.py
+++ perceprton.py
@@ -21,24 +21,44 @@ def productsum(input, weight):
         sum=sum+(input[i]*weight[i])
         #print(sum)
     return sum
-
-def perceptron(data):
-    idexNumber=0
-    b=0
+idexNumber=0
+givenResult=[]
+def perceptron(data,weight,predyy,LearningRate):
+    global idexNumber
+    global givenResult
+    b=0.1
     f=0
     c=0
-    weight=[1,1,1,1,1,1,1,1]
+    test=0    
     for index in range(0,768):
-    #print(data)
+        
+        test=test+1
+        #print(data)
+        
         inputLayer=data[index][:8]
-    #print(inputLayer)
+        #print(inputLayer)
+     
         TestResult=data[index][-1:]
-    #print(result)
-        result=productsum(inputLayer,weight)+b
+        givenResult=givenResult+TestResult
         #print(result)
-        b,f,c=trainThreshold(TestResult,345,result,b,c,f)
-        #print (b)
-    print (c/769)
+        if (idexNumber>0):
+       
+            for j in range(0,8):
+                #print(predyy[index])
+                #print(givenResult)
+                #print(test)
+                weight[j]=weight[j]-LearningRate*(predyy[index]-givenResult[index])*inputLayer[j]
+                
+
+
+        result=productsum(inputLayer,weight)+b
+            #print(result)
+        b,f,c,predy=trainThreshold(TestResult,345,result,b,c,f)
+    idexNumber+=1
+   
+        
+    print (c/768)
+    return predy
   
 
 def activation(result,threshold):
@@ -51,9 +71,12 @@ def activation(result,threshold):
     #print(f"Result: {result}, Threshold: {threshold}")
     #print(y)
     return y
-
+predy=[]
 def trainThreshold(TestResult,threshold,result,b,c,f):
+    global predy
     idk=activation(result,threshold)
+    predy=predy+idk
+    #print(predy)
     if (idk!=TestResult):
         #print(type(TestResult))
         #print(f"idk: {idk}, testresult: {TestResult}")
@@ -63,7 +86,16 @@ def trainThreshold(TestResult,threshold,result,b,c,f):
     else:
         c=c+1
         #print(f)
-    return b,f,c
+    return b,f,c,predy
+
+def training(data,LearningRate,epoche,weight):
+    predy=[]
+    for i in range(0,epoche):
+       predy=perceptron(data,weight,predy,LearningRate)
+       #print(predy)
+
+     
+
 
 
 
@@ -80,6 +112,7 @@ def trainThreshold(TestResult,threshold,result,b,c,f):
 if __name__=="__main__":
     filename='C:\\Users\\Jack\\Desktop\\Deep learning\\a1 perceptron\\diabetes.csv'
     data=readFile(filename)
-    perceptron(data)
+    weight=np.random.rand(8)
+    training(data,0.01,100,weight)
 
     
